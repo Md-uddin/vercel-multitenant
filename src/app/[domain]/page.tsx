0563d1eed5482714859d418042d221fe5f284583
Md-uddin <easeanger@gmail.com>
2024-05-29 21:01:33 +0530

    removed getstaticpaths for now

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,17 +1,6 @@
 // pages/[domain].tsx
 
-export async function getStaticPaths() {
-	const allUsers = [{ domains: ["demo", "weird"] }];
-	const allPaths = allUsers.flatMap((user) =>
-		user.domains.map((subdomain) => ({
-			params: {
-				domain: subdomain,
-			},
-		}))
-	);
 
-	return { paths: allPaths, fallback: false };
-}
 
 export default function DomainPage({ params }: { params: { domain: string } }) {
 	console.log({ params });
